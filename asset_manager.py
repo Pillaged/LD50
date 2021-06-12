acdde695f694e1f4d52feb046c0b0c1072dbb80e
Mason Kohls <mason.kohls1999@gmail.com>
2021-06-12 10:09:45 -0700

    Added audio loader

--- asset_manager.py
+++ asset_manager.py
@@ -1,6 +1,10 @@
 import pygame
+import logging
 
 import prepare
+from pygame import mixer
+
+logger = logging.getLogger(__name__)
 
 
 def transform_resource_filename(*filename):
@@ -13,10 +17,24 @@ def transform_resource_filename(*filename):
 
 def load_image(filename):
     """ Load image from the resources folder
-
-    * Filename will be transformed to be loaded from game resource folder
-    * Will be converted if needed.
     """
 
     filename = transform_resource_filename(filename)
     return pygame.image.load(filename)
+
+
+class DummySound:
+    def play(self):
+        pass
+
+
+def load_audio(filename):
+    """ Load sound from the resources folder
+    """
+    filename = transform_resource_filename(filename)
+    try:
+        return mixer.Sound(filename)
+    except pygame.error as e:
+        logger.error(e)
+        logger.error('Unable to load sound: {}'.format(filename))
+        return DummySound()
